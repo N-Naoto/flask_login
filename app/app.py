8d164facfb7dec6874bd84c7ac678e28e5c03bd5
N-Naoto <nao9909.n@gmail.com>
2021-03-10 08:47:29 +0900

    コメント追加

--- app/app.py
+++ app/app.py
@@ -12,11 +12,13 @@ app.secret_key = key.SECRET_KEY
 @app.route("/")
 @app.route("/index")
 def index():
+    # sessionの中にユーザー名があればindex.htmlを返す
     if "user_name" in session:
         name = session["user_name"]
         all_onegai = OnegaiContent.query.all()
         title_name = 'index'
         return render_template("index.html", name=name, all_onegai=all_onegai, title_name=title_name)
+    # ないならtop.htmlを返す
     else:
         return redirect(url_for("top", status="logout"))
 
@@ -50,21 +52,23 @@ def delete():
     return redirect(url_for("index"))
 
 
+# 初期画面
 @app.route("/top")
 def top():
     status = request.args.get("status")
     title_name = 'top'
     return render_template("top.html", status=status,title_name=title_name)
 
-
+# ログインボタンを押したら発火
 @app.route("/login", methods=["post"])
 def login():
     user_name = request.form["user_name"]
     user = User.query.filter_by(user_name=user_name).first()
+    # ユーザがいるなら
     if user:
         password = request.form["password"]
-        hashed_password = sha256(
-            (user_name + password + key.SALT).encode("utf-8")).hexdigest()
+        hashed_password = sha256((user_name + password + key.SALT).encode("utf-8")).hexdigest()
+        # パスワードが一致したら
         if user.hashed_password == hashed_password:
             session["user_name"] = user_name
             return redirect(url_for("index"))
@@ -73,14 +77,14 @@ def login():
     else:
         return redirect(url_for("top", status="user_notfound"))
 
-
+# 新規登録画面に移動するとき発火
 @app.route("/newcomer")
 def newcomer():
     status = request.args.get("status")
     title_name = 'newcomer'
     return render_template("newcomer.html", status=status, title_name=title_name)
 
-
+# 新規登録ボタンを押したら発火
 @app.route("/registar", methods=["post"])
 def registar():
     user_name = request.form["user_name"]
@@ -89,15 +93,14 @@ def registar():
         return redirect(url_for("newcomer", status="exist_user"))
     else:
         password = request.form["password"]
-        hashed_password = sha256(
-            (user_name + password + key.SALT).encode("utf-8")).hexdigest()
+        hashed_password = sha256((user_name + password + key.SALT).encode("utf-8")).hexdigest()
         user = User(user_name, hashed_password)
         db_session.add(user)
         db_session.commit()
         session["user_name"] = user_name
         return redirect(url_for("index"))
 
-
+# ログアウトボタンを押したら発火
 @app.route("/logout")
 def logout():
     session.pop("user_name", None)
