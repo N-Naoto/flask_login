36c2c31979778e43d151e46a139c736334e4ec9f
N-Naoto <nao9909.n@gmail.com>
2021-03-09 16:26:09 +0900

    データベースの中身を画面上に表示

--- app/app.py
+++ app/app.py
@@ -1,19 +1,21 @@
 from flask import Flask,render_template,request
 
+from models.models import OnegaiContent
+
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
     name = request.args.get('name')
-    words = ['月','火','水','木','金','土','日']
-    return render_template('index.html',name=name,words=words)
+    all_onegai = OnegaiContent.query.all()
+    return render_template('index.html',name=name,all_onegai=all_onegai)
 
 @app.route('/index',methods=['post'])
 def post():
     name = request.form['name']
-    words = ['月','火','水','木','金','土','日']
-    return render_template('index.html',name=name,words=words)
+    all_onegai = OnegaiContent.query.all()
+    return render_template('index.html',name=name,all_onegai=all_onegai)
 
 if __name__ == '__main__':
     app.run()
