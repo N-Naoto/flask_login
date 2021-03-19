cb53645bad18e2af1eb0a54c2127a469f4252dbc
N-Naoto <nao9909.n@gmail.com>
2021-03-19 23:35:56 +0900

    database.pyの変更 指定名

--- models/database.py
+++ models/database.py
@@ -13,11 +13,16 @@ import os
 
 # mysqlバージョン
 database_file = 'mysql+pymysql://{user}:{password}@{host}/{db}?charset=utf8'.format(**{
-    'user': os.getenv('DB_USER', os.environ['bf67e3e630aa27']),
+    'user': os.getenv('DB_USERNAME', os.environ['bf67e3e630aa27']),
     'password': os.getenv('DB_PASSWORD', os.environ['6d17926a']),
     'host': os.getenv('DB_HOST', os.environ['us-cdbr-east-03.cleardb.com']),
-    'db': os.getenv('DB_NAME', os.environ['heroku_820972e84f2e8c3'])
+    'db': os.getenv('DB_DATABASE', os.environ['heroku_820972e84f2e8c3'])
 })
+# database_file = 'mysql+pymysql://{user}:{password}@{host}/todo?charset=utf8'.format(**{
+#     'user': os.getenv('DB_USER', os.environ['user1']),
+#     'password': os.getenv('DB_PASSWORD', os.environ['user1']),
+#     'host': os.getenv('DB_HOST', os.environ['localhost']),
+# })
 engine = create_engine(database_file, encoding="utf-8", echo=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
