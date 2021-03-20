73063a6e09355350bb574ebb5a4b3a68c6f5b695
N-Naoto <nao9909.n@gmail.com>
2021-03-20 14:31:43 +0900

    localを動かす方をコメント解除

--- models/database.py
+++ models/database.py
@@ -12,17 +12,18 @@ import os
 # Base.query = db_session.query_property()
 
 # mysqlバージョン
-database_file = 'mysql+pymysql://{user}:{password}@{host}/{db}?charset=utf8'.format(**{
-    'user': os.getenv('DB_USERNAME', os.environ['b8e9ff724a7a6b']),
-    'password': os.getenv('DB_PASSWORD', os.environ['886f95f0']),
-    'host': os.getenv('DB_HOST', os.environ['us-cdbr-east-03.cleardb.com']),
-    'db': os.getenv('DB_DATABASE', os.environ['heroku_9e15f4d597e722d'])
-})
-# database_file = 'mysql+pymysql://{user}:{password}@{host}/todo?charset=utf8'.format(**{
-#     'user': os.getenv('DB_USER', os.environ['user1']),
-#     'password': os.getenv('DB_PASSWORD', os.environ['user1']),
-#     'host': os.getenv('DB_HOST', os.environ['localhost']),
+# database_file = 'mysql+pymysql://{user}:{password}@{host}/{db}?charset=utf8'.format(**{
+#     'user': os.getenv('DB_USERNAME', os.environ['b8e9ff724a7a6b']),
+#     'password': os.getenv('DB_PASSWORD', os.environ['886f95f0']),
+#     'host': os.getenv('DB_HOST', os.environ['us-cdbr-east-03.cleardb.com']),
+#     'db': os.getenv('DB_DATABASE', os.environ['heroku_9e15f4d597e722d'])
 # })
+database_file = 'mysql+pymysql://{user}:{password}@{host}/todo?charset=utf8'.format(**{
+    'user': os.getenv('DB_USER', os.environ['user1']),
+    'password': os.getenv('DB_PASSWORD', os.environ['user1']),
+    'host': os.getenv('DB_HOST', os.environ['localhost']),
+})
+
 engine = create_engine(database_file, encoding="utf-8", echo=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
