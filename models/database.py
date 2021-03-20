a3ad32b91a1bdbba471e60d6dd68a2d4d86dda46
N-Naoto <nao9909.n@gmail.com>
2021-03-20 15:07:28 +0900

    database.pyの検証中 デプロイできてないから。

--- models/database.py
+++ models/database.py
@@ -12,13 +12,13 @@ import os
 # Base.query = db_session.query_property()
 
 # mysqlバージョン
-database_file = os.getenv('CLEARDB_DATABASE_URL')
-# database_file = 'mysql+pymysql://{user}:{password}@{host}/{db}?charset=utf8'.format(**{
-#     'user': os.getenv('DB_USERNAME', os.environ['b8e9ff724a7a6b']),
-#     'password': os.getenv('DB_PASSWORD', os.environ['886f95f0']),
-#     'host': os.getenv('DB_HOST', os.environ['us-cdbr-east-03.cleardb.com']),
-#     'db': os.getenv('DB_DATABASE', os.environ['heroku_9e15f4d597e722d'])
-# })
+# database_file = os.getenv('CLEARDB_DATABASE_URL')
+database_file = 'mysql+pymysql://{user}:{password}@{host}/{db}?charset=utf8'.format(**{
+    'user': os.getenv('DB_USERNAME', b8e9ff724a7a6b),
+    'password': os.getenv('DB_PASSWORD', 886f95f0),
+    'host': os.getenv('DB_HOST', us-cdbr-east-03.cleardb.com),
+    'db': os.getenv('DB_DATABASE', heroku_9e15f4d597e722d)
+})
 # database_file = 'mysql+pymysql://{user}:{password}@{host}/todo?charset=utf8'.format(**{
 #     'user': os.getenv('DB_USER','user1'),
 #     'password': os.getenv('DB_PASSWORD','user1'),
